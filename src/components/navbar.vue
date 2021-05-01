10ba6af0703ceb5ef09cedf3b8444417ced77ea1
ciarnellicarlo <ciarnellicarloedoardo@gmail.com>
2021-05-01 18:16:53 +0200

    styling navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>this is navbar</div>
+    <div class="navbar">
+        <div class="logo">logo</div>
+        <div class="login">login</div>
+        <div class="register">register</div>
+        <div class="footer">footer example</div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +14,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .navbar {
+        background-color: black;
+        height: 100vh;
+        color: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        position: relative;
+    }
 
+    .footer {
+        position: absolute;
+        top: 92vh;
+    }
 </style>
