4293940cc8fd1f4f75bb5c682103cc2fc5d668e1
ciarnellicarlo <ciarnellicarloedoardo@gmail.com>
2021-05-01 17:36:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <navbar />
+    <div class="content">
+      <slider />
+      <div class="blog">
+        blog part 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import navbar from './components/navbar.vue';
+import slider from './components/slider.vue';
+
+export default {
+  name: 'DeloitteTask',
+  components: {
+    navbar,
+    slider
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>this is navbar</div>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
