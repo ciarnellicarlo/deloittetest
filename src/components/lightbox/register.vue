b4790fc5e25561b7db3252410780eaa83d4aba52
ciarnellicarlo <ciarnellicarloedoardo@gmail.com>
2021-05-06 19:20:04 +0200

    mobile and dob validation

--- src/components/lightbox/register.vue
+++ src/components/lightbox/register.vue
@@ -13,38 +13,38 @@
                         <div class="loginpicture col-md-6">
                             <img src="@/assets/register.jpg" alt="">
                         </div>
-                        <form class="col-md-6">
+                        <form class="col-md-6" v-on:submit.prevent="onSubmit">
                             <div class="form-group">
                                 <label for="InputName1">Name</label>
-                                <input type="text" class="form-control" id="InputName1" aria-describedby="nameHelp" placeholder="Enter name" v-model="name">
+                                <input type="text" class="form-control" id="InputName1" aria-describedby="nameHelp" placeholder="Enter name" v-model="name" required>
                             </div>
                             <div class="form-group">
                                 <label for="InputSurname1">Surname</label>
-                                <input type="text" class="form-control" id="InputSurname1" aria-describedby="surnameHelp" placeholder="Enter Surname" v-model="surname">
+                                <input type="text" class="form-control" id="InputSurname1" aria-describedby="surnameHelp" placeholder="Enter Surname" v-model="surname" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Mobile</label>
-                                <input type="tel" class="form-control" id="InputMobile1" aria-describedby="mobileHelp" placeholder="+356 xx123456" v-model="tel">
+                                <label for="InputMobile1">Mobile</label>
+                                <input type="tel" class="form-control" id="InputMobile1" aria-describedby="mobileHelp" placeholder="+356 xx123456" v-model="tel" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email</label>
-                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email">
+                                <label for="InputEmail1">Email</label>
+                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Birthday</label>
-                                <input type="date" class="form-control" id="exampleInputPassword1" placeholder="Birthday" v-model="birthday">
+                                <label for="InputBirthday1">Birthday</label>
+                                <input type="date" class="form-control" id="InputBirthday1" v-model="birthday" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+                                <label for="InputPassword1">Password</label>
+                                <input type="password" class="form-control" id="InputPassword1" placeholder="Password" v-model="password" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Username</label>
-                                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Username" v-model="username">
+                                <label for="InputUsername1">Username</label>
+                                <input type="text" class="form-control" id="InputUsername1" placeholder="Username" v-model="username" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Address</label>
-                                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Address" v-model="address">
+                                <label for="InputAddress1">Address</label>
+                                <input type="text" class="form-control" id="InputAddress1" placeholder="Address" v-model="address">
                             </div>
                             <button type="submit" class="btn btn-dark">Register</button>
                         </form>
@@ -68,6 +68,19 @@ export default {
             password: '',
             username: '',
             address: ''
+        }
+    },
+
+    methods: {
+        onSubmit() {
+            if (this.tel.length !== 13) {
+                alert("Mobile number must be 13 digits long. Please try again")
+            } 
+            var today = new Date()
+            var now = new Date(this.birthday);
+            if (now > today) {
+                alert("Your birthday is in the future")
+            }
         }
     }
 }
