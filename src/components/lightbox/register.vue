524371847291bcd7fd0e0e82e3786d089c92cf4b
ciarnellicarlo <ciarnellicarloedoardo@gmail.com>
2021-05-07 20:58:17 +0200

    alert boxes for registration

--- src/components/lightbox/register.vue
+++ src/components/lightbox/register.vue
@@ -15,32 +15,44 @@
                         </div>
                         <form class="col-md-6" v-on:submit.prevent="registration">
                             <div class="form-group">
-                                <label for="InputName1">Name</label>
+                                <label for="InputName1">Name</label><small class="asterisk"> *</small>
                                 <input type="text" class="form-control" id="InputName1" aria-describedby="nameHelp" placeholder="Enter name" v-model="name" required>
                             </div>
                             <div class="form-group">
-                                <label for="InputSurname1">Surname</label>
+                                <label for="InputSurname1">Surname</label><small class="asterisk"> *</small>
                                 <input type="text" class="form-control" id="InputSurname1" aria-describedby="surnameHelp" placeholder="Enter Surname" v-model="surname" required>
                             </div>
                             <div class="form-group">
-                                <label for="InputMobile1">Mobile</label>
+                                <label for="InputMobile1">Mobile</label><small class="asterisk"> *</small>
                                 <input type="tel" class="form-control" id="InputMobile1" aria-describedby="mobileHelp" placeholder="+356 xx123456" v-model="tel" required>
+                                <div class="invalid-feedback">
+                                    Mobile number must be 13 digits long. Please try again.
+                                </div>
                             </div>
                             <div class="form-group">
-                                <label for="InputEmail1">Email</label>
+                                <label for="InputEmail1">Email</label><small class="asterisk"> *</small>
                                 <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email" v-model="email" required>
                             </div>
                             <div class="form-group">
-                                <label for="InputBirthday1">Birthday</label>
+                                <label for="InputBirthday1">Birthday</label><small class="asterisk"> *</small>
                                 <input type="date" class="form-control" id="InputBirthday1" v-model="birthday" required>
+                                <div class="invalid-feedback">
+                                    Date of birth cannot be in the future. Please try again.
+                                </div>
                             </div>
                             <div class="form-group">
-                                <label for="InputPassword1">Password</label>
+                                <label for="InputPassword1">Password</label><small class="asterisk"> *</small>
                                 <input type="password" class="form-control" id="InputPassword1" placeholder="Password" v-model="password" required>
+                                <div class="invalid-feedback">
+                                    Password needs to contain at least 8 characters, 1 capital letter, 1 small character, 1 digit and 1 special character (!@#$%^&*)
+                                </div>
                             </div>
                             <div class="form-group">
-                                <label for="InputUsername1">Username</label>
+                                <label for="InputUsername1">Username</label><small class="asterisk"> *</small>
                                 <input type="text" class="form-control" id="InputUsername1" placeholder="Username" v-model="username" required>
+                                <div class="invalid-feedback">
+                                    This username is already in use. Please try another one.
+                                </div>
                             </div>
                             <div class="form-group">
                                 <label for="InputAddress1">Address</label>
@@ -75,22 +87,28 @@ export default {
         registration() {
             var today = new Date()
             var now = new Date(this.birthday);
-            var str = this.password;	// your password field's value goes here
+            var str = this.password;
             var re = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/
             if (this.tel.length !== 13) {
-                alert("Mobile number must be 13 digits long. Please try again")
+                document.getElementById("InputMobile1").classList.add("is-invalid");
             } 
             
             else if (now > today) {
-                alert("Your birthday is in the future")
+                document.getElementById("InputBirthday1").classList.add("is-invalid");
+                document.getElementById("InputMobile1").classList.remove("is-invalid");
             }
             
             else if (re.test(str) == false) {
-                alert("Password needs to contain at least 8 characters, 1 capital letter, 1 small character, 1 digit and 1 special character (!@#$%^&*)")
+                document.getElementById("InputPassword1").classList.add("is-invalid");
+                document.getElementById("InputBirthday1").classList.remove("is-invalid");
+                document.getElementById("InputMobile1").classList.remove("is-invalid");
             }
 
             else {
-                this.saveData()
+                this.saveData();
+                document.getElementById("InputMobile1").classList.remove("is-invalid");
+                document.getElementById("InputBirthday1").classList.remove("is-invalid");
+                document.getElementById("InputPassword1").classList.remove("is-invalid");
             }
         },
 
@@ -125,11 +143,12 @@ export default {
                 }
 
                 if ( found ) {
-                    alert("This username is already in use. Please try another.");
+                    document.getElementById("InputUsername1").classList.add("is-invalid");
                 } else {
                         accounts.push(account);
                         localStorage.setItem('accounts', JSON.stringify(accounts));
                         alert("You have successfully registered. You can now login.");
+                        document.getElementById("InputUsername1").classList.remove("is-invalid");
                     }
             }
         }
@@ -147,6 +166,10 @@ export default {
         flex-direction: column;
         justify-content: center;
         max-height: 850px;
+
+        .asterisk {
+            color: red;
+        }
 
         .btn {
             background-color: #86BC25;
